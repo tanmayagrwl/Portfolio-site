2450188934bc19b0efef7a1cdcd2a777bbddcc2c
tanmayagrwl <tanmayhp78@gmail.com>
2023-02-05 20:22:19 +0530

    Complete about.html

---
+++ about.js
@@ -0,0 +1,7 @@
+document.getElementById("navlink").addEventListener("mouseover", function(){
+    this.innerHTML = "Tanmay.Home";
+    });
+    document.getElementById("navlink").addEventListener("mouseout", function(){
+    this.innerHTML = "Tanmay.";
+    });
+    
