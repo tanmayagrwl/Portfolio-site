91fcbe85f89c28194d83397637f67ec479ebf4e0
tanmayagrwl <tanmayhp78@gmail.com>
2023-02-05 15:39:02 +0530

    complete index.html

---
+++ script.js
@@ -0,0 +1,21 @@
+document.getElementById("x").addEventListener("mouseover", function(){
+    this.innerHTML = "About";
+  });
+  document.getElementById("x").addEventListener("mouseout", function(){
+    this.innerHTML = "Hello.";
+  });
+
+  document.getElementById("y").addEventListener("mouseover", function(){
+    this.innerHTML = "Work";
+  });
+  document.getElementById("y").addEventListener("mouseout", function(){
+    this.innerHTML = "I am";
+  });
+
+  document.getElementById("z").addEventListener("mouseover", function(){
+    this.innerHTML = "Contact";
+  });
+  document.getElementById("z").addEventListener("mouseout", function(){
+    this.innerHTML = "Tanmay";
+  });
+  
